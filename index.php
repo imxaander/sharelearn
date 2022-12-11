74c7dea028b3edf9f1a71d0175f05ba4ee138e03
imxaander <xander22.ison@gmail.com>
2022-12-11 08:54:52 +0800

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/index.css">
+    <title>ShareLearn - Paranaque National Highschool - Main</title>
+</head>
+<body>
+    <div id="top-bar">
+        <p>PARANAQUE NATIONAL HIGHSCHOOL - MAIN</p>
+    </div>
+</body>
+</html>
